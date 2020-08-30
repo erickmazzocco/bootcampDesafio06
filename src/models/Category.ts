4afea1a51ba0a8597c4067071027932f672f9799
Érick Mazzocco <erick.mazzocco@elogroup.com.br>
2020-08-30 12:14:13 -0300

    javascript é gambiarra

---
+++ src/models/Category.ts
@@ -0,0 +1,24 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('categories')
+class Category {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'title' })
+  title: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  created_at: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updated_at: Date;
+}
+
+export default Category;
